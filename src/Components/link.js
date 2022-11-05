d8f956372b7c6aab0cecd12d7313bd7dc712eec4
oluwasegun31 <oluwasegun1412@gmail.com>
2022-11-05 01:14:09 +0100

    fix: fixed anchor redirecting tag

--- src/Components/link.js
+++ src/Components/link.js
@@ -2,24 +2,23 @@ import React from "react";
 import dataLink from "../Data/data";
 import './link.css'
 import github from '../Images/Social icon.svg'
-import slack from '../Images/slack.svg'
-import { Link } from "react-router-dom";
+import slack from '../Images/slack.svg';
+
 function Links(){
     const dataItem = dataLink.map((items, pos)=> {
         return(
-          <div className='btn' key={pos}>
-            <a id={items.id} href={items.link}>{items.title}</a>
-          </div>
-              
+            <div className='btn' key={pos}>
+                <a id={items.id} href={items.link}>{items.title}</a>
+            </div>     
         )
     })
     return(
         <main>
             {dataItem}
-            <Link to='/contact'>
-                <a id='contact' href='/contact'>Contact Me</a>
-            </Link>
-            
+
+            <div className="btn-2">
+                <a id="contact" href="/contact" >Contact me</a>
+            </div>
             <div className="social-links">
                 <img src={github} alt='Github' />
                 <img src={slack} alt='Slack' />
